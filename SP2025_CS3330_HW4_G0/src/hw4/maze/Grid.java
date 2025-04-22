dc1d40bbf3f4793e6cc3fbe5b346776eda03c255
Turner Van Duser <turner.vanduser@gmail.com>
2025-04-22 18:10:19 -0500

    finished writing the rows.java, still need to write out all of the javadoc

--- SP2025_CS3330_HW4_G0/src/hw4/maze/Grid.java
+++ SP2025_CS3330_HW4_G0/src/hw4/maze/Grid.java
@@ -1,5 +1,27 @@
 package hw4.maze;
 
+import java.util.ArrayList;
+
 public class Grid {
+
+    private ArrayList<Row> rows;
+
+	public Grid(ArrayList<Row> rows) {
+        this.rows = rows;
+	}
+
+	public Object getRows() {
+		return rows;
+	}
+
+	public void setRows(ArrayList<Row> rows) {
+		// TODO Auto-generated method stub    	
+        this.rows = rows; 
+	}
+
+    @Override
+    public String toString() {
+        return "Grid [rows=" + rows + "]";
+    }
 
 }
